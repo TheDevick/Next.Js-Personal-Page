0fc1c501b1dddaaec5deb4b3832eaf109c5bdb54
DinosDev <erick@bilhalba.com.br>
2022-02-12 23:56:23 -0300

    feat(project-configuration): create HomePage

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <div>
+        <h1>Home Page</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+            repellendus.
+        </p>
+        </div>
+    );
+}
