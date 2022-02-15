2a535729f960bfbf5cd514a92fe56dc4ff5fd02e
DinosDev <erick@bilhalba.com.br>
2022-02-15 05:35:14 -0300

    feat: create Description/About Component

---
+++ src/components/Description/About.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const About = styled.p`
+    margin-top: 20px;
+
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 28px;
+
+    letter-spacing: 0.45px;
+
+    color: #4A4A4A;
+
+    @media (max-width: 992px) {
+        margin: 20px 5px 0 5px;
+    }
+`
+
+export default About;
--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Subtitle from "./Subtitle"
+import About from "./About"
+import GithubLink from "../GithubLink"
 
 const StyledDiv = styled.div`
     margin: 65px 151px 28px 151px;
@@ -9,10 +11,34 @@ const StyledDiv = styled.div`
     }
 `
 
+const StyledLink = styled.a`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 28px;
+
+    letter-spacing: 0.45px;
+
+    color: #4A4A4A;
+`
+
 export default function Description() {
     return (
         <StyledDiv>
             <Subtitle>Hi 👋, I'm Erick, a 15 Years Old Brazilian Developer! Currently Studying Lumen (Laravel) and Next.Js.</Subtitle>
+
+            <About>
+                With a Lot of Projects in <GithubLink>Github</GithubLink>, I Love to Explore New Things.
+                <br />
+                Solid Experiences With {' '}
+                <StyledLink href="https://git-scm.com/">Git</StyledLink>, {' '}
+                <StyledLink href="https://github.com/">Github</StyledLink> and {' '}
+                <StyledLink href="https://pt.wikipedia.org/wiki/Linux">Linux</StyledLink>, A Little Bit of {' '}
+                <StyledLink href="https://www.docker.com/">Docker</StyledLink>, {' '}
+                <StyledLink href="https://www.python.org/">Python</StyledLink>, {' '}
+                <StyledLink href="https://unity.com/">C# (Unity)</StyledLink>, And Of Course, {' '}
+                <StyledLink href="https://lumen.laravel.com/">Lumen</StyledLink> and <StyledLink href="https://nextjs.org/">Next.js</StyledLink>!
+            </About>
         </StyledDiv>
     )
 }
