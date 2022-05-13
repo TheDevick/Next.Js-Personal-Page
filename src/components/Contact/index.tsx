05a9cfe194948330fb5f8b386566294b29651425
DinosDev <erick@bilhalba.com.br>
2022-05-13 15:26:38 -0300

    feat: Add Styled Text To Contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -9,10 +9,31 @@ const StyledDiv = styled.div`
     }
 `
 
+const StyledText = styled.p`
+    white-space: pre-wrap;
+    margin-top: 20px;
+
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 20px;
+
+    letter-spacing: 0.45px;
+
+    color: #4A4A4A;
+
+    @media (max-width: 992px) {
+        margin: 20px 5px 0 5px;
+        text-align: center;
+        line-height: 50px;
+    }
+`
+
 export default function Contact() {
     return (
         <StyledDiv id="Contact">
             <Title>Contact</Title>
+            <StyledText>You can text me on Github, Linkedin Or Twitter.</StyledText>
         </StyledDiv>
     )
 }
