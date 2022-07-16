b0c9b12ad4fef3a9dc3e1d7683c4680566067dc3
The Devick <71853418+TheDevick@users.noreply.github.com>
2022-07-16 02:12:10 -0300

    Update index.tsx

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -37,7 +37,7 @@ export default function Description() {
                 <StyledLink href="https://www.docker.com/">Docker</StyledLink>, {' '}
                 <StyledLink href="https://www.python.org/">Python</StyledLink>, {' '}
                 <StyledLink href="https://unity.com/">C# (Unity)</StyledLink>, And Of Course, {' '}
-                <StyledLink href="https://laravel.com/">Laravel</StyledLink> and <StyledLink href="https://nextjs.org/">Next.js</StyledLink>!
+                <StyledLink href="https://laravel.com/">Laravel</StyledLink> and <StyledLink href="https://https://symfony.com//">Symfony</StyledLink>!
             </About>
         </StyledDiv>
     )
